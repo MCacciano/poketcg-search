9539df4b769697beb46434a564055361514d1b3d
Michael <michael.cacciano@gmail.com>
2020-07-25 22:42:25 -0400

    pulling cards down from api with images

--- src/App.js
+++ src/App.js
@@ -1,26 +1,36 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from 'react';
 
-function App() {
+import './App.css';
+import pokeTcg from './axios/pokeTcg';
+
+const App = () => {
+  const [cards, setCards] = useState(null);
+
+  useEffect(() => {
+    try {
+      const getCards = async () => {
+        const { data } = await pokeTcg.get('/cards');
+        setCards(data.cards);
+      };
+
+      getCards();
+    } catch (err) {
+      console.error(err);
+    }
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <ul className="grid m-5 gap-10 grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5">
+        {cards &&
+          cards.map(card => (
+            <li>
+              <img className="w-full h-auto" src={card.imageUrl} />
+            </li>
+          ))}
+      </ul>
     </div>
   );
-}
+};
 
 export default App;
