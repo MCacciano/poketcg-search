681d64df4b4298daae56fb3b2a4366c7e98beb3a
Michael <michael.cacciano@gmail.com>
2020-07-28 22:36:21 -0400

    working on types

--- src/components/TypesList.js
+++ src/components/TypesList.js
@@ -1,43 +1,34 @@
-import React, { useState } from 'react';
+import React, { useState, useRef } from 'react';
 
-const TypesList = ({ types, onChange, checked = false }) => {
-  const typeColors = {
-    colorless: '#B0B0B0',
-    darkness: '#333F65',
-    dragon: '#C0A534',
-    fairy: '#FF3DA3',
-    fighting: '#C74C00',
-    fire: '#B70A03',
-    grass: '#12961F',
-    lightning: '#F6E300',
-    metal: '#728788',
-    psychic: '#7200BA',
-    water: '#054BBD',
+const TypesList = ({ types, onChange }) => {
+  const [isActive, setIsActive] = useState(false);
+
+  const handleOnClick = (e, type) => {
+    const classType = type.toLowerCase();
+    const { classList } = e.target;
+
+    if (e.target.checked) {
+      classList.toggle(`bg-${classType}`);
+      classList.replace(`text-${classType}`, `text-white`);
+    } else {
+      classList.toggle(`bg-white`);
+      classList.replace(`text-white`, `text-${classType}`);
+    }
   };
 
-  const labelClasses = `relative flex py-1 px-2 border rounded shadow cursor-pointer transition ease-in-out duration-300 transform hover:-translate-y-1 hover:scale-110`;
-
-  const [isChecked, setIsChecked] = useState(false);
-
   return (
-    <div className='w-full flex justify-between my-5 px-5'>
+    <div className="w-full flex justify-between my-5 px-5">
       {types.map(type => (
         <label
-          style={{
-            borderColor: `${typeColors[type.toLowerCase()]}`,
-            color: `${isChecked ? '#fff' : typeColors[type.toLowerCase()]}`,
-            backgroundColor: `${
-              !isChecked ? '#fff' : typeColors[type.toLowerCase()]
-            }`,
-          }}
-          className={labelClasses}
-          onClick={e => setIsChecked(!isChecked)}
+          key={type}
+          className={`relative flex py-1 px-2 border rounded shadow cursor-pointer hover:bg-${type.toLowerCase()} text-${type.toLowerCase()} border-${type.toLowerCase()} hover:text-white hover:bg-${type.toLowerCase()}`}
+          onClick={e => handleOnClick(e, type)}
         >
           {type}
           <input
-            className='hidden'
-            type='checkbox'
-            name='pokemonType'
+            className="hidden"
+            type="checkbox"
+            name="pokemonType"
             value={type}
             onChange={onChange}
           />
